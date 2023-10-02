3fa37bbf148285f306aa7bd668fc698268b2d761
ArtLevel <algitIT@rambler.ru>
2023-10-02 11:06:21 +0300

    New HW8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,21 @@
-import {UserType} from '../HW8'
+import { UserType } from '../HW8'
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+	| { type: 'sort'; payload: 'up' | 'down' }
+	| { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => {
+	switch (action.type) {
+		case 'sort':
+			const stateCopy = [...state]
+			if (action.payload === 'up') stateCopy.sort((a, b) => a.name > b.name ? 1 : 0)
+			if (action.payload === 'down') stateCopy.sort((a, b) => a.name < b.name ? 1 : 0)
+			return stateCopy
+		case 'check': {
 
-            return state // need to fix
-        }
-        case 'check': {
-
-            return state // need to fix
-        }
-        default:
-            return state
-    }
+			return state // need to fix
+		}
+		default:
+			return state
+	}
 }
