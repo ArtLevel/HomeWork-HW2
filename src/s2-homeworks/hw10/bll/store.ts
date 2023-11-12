417f689e981b65b463e4490301239a5488eff216
ArtLevel <algitIT@rambler.ru>
2023-11-12 12:28:14 +0300

    HW 10 completed

--- src/s2-homeworks/hw10/bll/store.ts
+++ src/s2-homeworks/hw10/bll/store.ts
@@ -3,12 +3,11 @@ import { combineReducers, legacy_createStore } from 'redux'
 import { themeReducer } from '../../hw12/bll/themeReducer'
 
 const reducers = combineReducers({
-    loading: loadingReducer, // hw10
-    theme: themeReducer, // hw12
+	loading: loadingReducer, // hw10
+	theme: themeReducer // hw12
 })
 
 const store = legacy_createStore(reducers)
-
 export default store
 
 export type AppStoreType = ReturnType<typeof reducers>
